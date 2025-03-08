49d7ff4a9a35c5cea007715636ed560a718641b6
Bebs Fiorillo <estevaovrb2014@gmail.com>
2025-03-08 10:08:29 -0300

    classe pais inicial finalizada

--- src/main/java/Pais.java
+++ src/main/java/Pais.java
@@ -1,2 +1,31 @@
 public class Pais {
+    private String nome;
+    private String sigla;
+
+    public Pais(String nome, String sigla) {
+        this.setNome(nome);
+        this.setSigla(sigla);
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        if (nome == null || nome.isEmpty()) {
+            throw new IllegalArgumentException("País deve possuir um nome.");
+        }
+        this.nome = nome;
+    }
+
+    public String getSigla() {
+        return sigla;
+    }
+
+    public void setSigla(String sigla) {
+        if (sigla == null || sigla.isEmpty()) {
+            throw new IllegalArgumentException("Sigla deve possuir um nome.");
+        }
+        this.sigla = sigla;
+    }
 }
