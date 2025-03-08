d0f2253e9d438bc2865e9afa626ea3ea04d5bb7b
Bebs Fiorillo <estevaovrb2014@gmail.com>
2025-03-08 15:20:00 -0300

    cria classe abstrata pessoa inicial

---
+++ src/main/java/Pessoa.java
@@ -0,0 +1,58 @@
+public abstract class Pessoa {
+    private String nome;
+    private int ID;
+    private Cidade cidade;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        if(nome == null || nome.isEmpty()) {
+            throw new IllegalArgumentException("Pessoa deve ter nome.");
+        }
+        this.nome = nome;
+    }
+
+    public int getID() {
+        return ID;
+    }
+
+    public void setID(int ID) {
+        if (ID <= 0){
+            throw new IllegalArgumentException("Pessoa deve ter ID maior que 0 para que seja válido.");
+        }
+        this.ID = ID;
+    }
+
+    public void setCidade(Cidade cidade) {
+        if (cidade == null) {
+            throw new NullPointerException("Pessoa deve ter cidade.");
+        }
+        this.cidade = cidade;
+    }
+
+    public String retornarNomeCidade(){
+        return this.cidade.getNome();
+    }
+
+    public String retornarSiglaCidade(){
+        return this.cidade.getSigla();
+    }
+
+    public String retornarNomeEstado(){
+        return this.cidade.getNome();
+    }
+
+    public String retornarSiglaEstado(){
+        return this.cidade.getSigla();
+    }
+
+    public String retornarNomePais(){
+        return this.cidade.retornarNomePais();
+    }
+
+    public String retornarSiglaPais(){
+        return this.cidade.retornarSiglaPais();
+    }
+}
