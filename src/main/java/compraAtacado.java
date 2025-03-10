4cc20f112c1f1cf4bbb0373246097ce5372c1cde
Bebs Fiorillo <estevaovrb2014@gmail.com>
2025-03-10 00:28:26 -0300

    cria compraAtacado inicial

---
+++ src/main/java/compraAtacado.java
@@ -0,0 +1,51 @@
+public class compraAtacado extends OcorrenciaProduto{
+    private Parceiro destinatario;
+    private Parceiro remetente;
+
+    public compraAtacado(String descricao, int estoque, int qtdOcorrencia, float valorUnitario, Parceiro destinatario, Parceiro remetente) {
+        super(descricao, true, estoque, qtdOcorrencia, valorUnitario);
+        this.setDestinatario(destinatario);
+        this.setRemetente(remetente);
+    }
+
+    public Parceiro getRemetente() {
+        return remetente;
+    }
+
+    public void setRemetente(Parceiro remetente) {
+        if (remetente == null) {
+            throw new NullPointerException("Deve possuir um remetente!");
+        }
+        this.remetente = remetente;
+    }
+
+    public Parceiro getDestinatario() {
+        return destinatario;
+    }
+
+    public void setDestinatario(Parceiro destinatario) {
+        if (destinatario == null) {
+            throw new NullPointerException("Deve possuir um destinatário!");
+        }
+        this.destinatario = destinatario;
+    }
+
+    public float calcularDesconto(){
+        float desconto =0.1f;
+        if (this.getRemetente().getAnosParceria() > 5) {
+            desconto += 0.1f;
+        }
+        else{
+            desconto += 0.02f * this.getRemetente().getAnosParceria();
+        }
+        return desconto ;
+    }
+
+    public String retornarLocalEntrega(){
+        return ("O endereço de destinatário é a Cidade " + this.getDestinatario().retornarNomeCidade() + " no estado de " + this.getDestinatario().retornarNomeEstado() + " - " + this.getDestinatario().retornarSiglaPais());
+    }
+
+    public String retornarLocalRemetente(){
+        return ("O endereço do remetente é a Cidade " + this.getRemetente().retornarNomeCidade() + " no estado de " + this.getRemetente().retornarNomeEstado() + " - " + this.getRemetente().retornarSiglaPais());
+    }
+}
