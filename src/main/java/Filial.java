a444aec3f91137974c77726ef9b7bc685772e3c7
Bebs Fiorillo <estevaovrb2014@gmail.com>
2025-03-10 22:43:46 -0300

    cria classe filial inicial

---
+++ src/main/java/Filial.java
@@ -0,0 +1,45 @@
+public class Filial {
+    private Funcionario coordenadorLocal;
+    private Historico historico;
+    public Filial(Historico historico){
+        this.setHistorico(historico);
+    }
+
+    public Funcionario getCoordenadorLocal() {
+        if (coordenadorLocal == null) {
+            throw new IllegalArgumentException("Não há coordenador local na Filial!"); //como não é obrigatório, precisa de verificação no get.
+        }
+        return coordenadorLocal;
+    }
+
+    public void setCoordenadorLocal(Funcionario coordenadorLocal) {
+        this.coordenadorLocal = coordenadorLocal; //eh possivel ser nulo, coordenadorLocal não é obrigatório.
+    }
+
+    public Historico getHistorico() {
+        return historico;
+    }
+
+    public void setHistorico(Historico historico) {
+        if (historico == null) {
+            throw new NullPointerException("Filial deve possuir histórico!");
+        }
+        this.historico = historico;
+    }
+
+    public String imprimirHistorico() {
+        return this.getHistorico().imprimirDetalhado();
+    }
+
+    public String imprimirApenasDiscriminativo(){
+        return this.getHistorico().imprimirDescricoes();
+    }
+
+    public int qtdDependentesCoordenadorLocal(){
+        return this.getCoordenadorLocal().quantDeps();
+    }
+
+    public int qtdDepsAbonoCoordenadorLocal(){
+        return this.getCoordenadorLocal().quantDepAbono();
+    }
+}
