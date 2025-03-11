24f71ed17ec12fee09758b82f3bea1d81ac1d907
Bebs Fiorillo <estevaovrb2014@gmail.com>
2025-03-11 01:30:44 -0300

    finaliza consultas (19 e 20)

--- src/test/java/FilialTest.java
+++ src/test/java/FilialTest.java
@@ -28,25 +28,11 @@ class FilialTest {
     }
 
     @Test
-    void deveRetornarCoordenadorLocalDefinido(){
-        Funcionario p = new Funcionario("Fernando Silva",123,new Cidade ("Juiz de Fora", "JF",new Estado("Minas Gerais", "MG", new Pais("Brasil","BR"))),1412.0f,0.0f);
+    void deveRetornarCoordenadorLocalDefinido() {
+        Funcionario p = new Funcionario("Fernando Silva", 123, new Cidade("Juiz de Fora", "JF", new Estado("Minas Gerais", "MG", new Pais("Brasil", "BR"))), 1412.0f, 0.0f);
         Filial f = new Filial(new Historico());
         f.setCoordenadorLocal(p);
-        assertEquals(p,f.getCoordenadorLocal());
-    }
-
-    @Test
-    void deveImprimirApenasDiscriminativo(){
-        Historico h = new Historico();
-        folhaPagamento v = new folhaPagamento("Folha de Pagamento do Mês de Maio", 1.5f);
-        Cidade c = new Cidade ("Juiz de Fora", "JF",new Estado("Minas Gerais", "MG", new Pais("Brasil","BR")));
-        Empresa e = new Empresa("Empresa 1", 456, c,  0);
-        Parceiro j = new Parceiro("Empresa 2",1,c,0);
-        vendaAtacado g = new vendaAtacado("Venda de Lote de PCS", 20, 2, 2000f, j, e);
-        h.addOcorrencia(v);
-        h.addOcorrencia(g);
-        Filial f = new Filial(h);
-        assertEquals("Folha de Pagamento do Mês de Maio\nVenda de Lote de PCS\n",f.imprimirApenasDiscriminativo());
+        assertEquals(p, f.getCoordenadorLocal());
     }
 
     @Test
@@ -67,31 +53,6 @@ class FilialTest {
         assertEquals("Prejuízo de R$1313.5 - Folha de Pagamento do Mês de Maio\nLucro de R$3600.0 - Venda de Lote de PCS\nNão houve mudança no faturamento - Folha de Pagamento Vazia\n",f.imprimirHistorico());
     }
 
-    @Test
-    void deveCalcularLucroFinal(){
-        Historico h = new Historico();
-        Cidade c = new Cidade ("Juiz de Fora", "JF",new Estado("Minas Gerais", "MG", new Pais("Brasil","BR")));
-        Funcionario p = new Funcionario("Fernando Silva",123,c,1412.0f,100.0f);
-        Dependente p1 = new Dependente("João Srbek",1,c,17);
-        Dependente p2 = new Dependente("Caua Moreno",2,c,18);
-        Dependente p3 = new Dependente("Pedro Moreno Srbek",3,c,19);
-        p.addDependentes(p1);
-        p.addDependentes(p2);
-        p.addDependentes(p3);
-        Funcionario d = new Funcionario("Carlos",456,c,1412.0f,0.0f);
-        Dependente d1 = new Dependente("Carlos Junior",4,c,17);
-        d.addDependentes(d1);
-        folhaPagamento v = new folhaPagamento("Folha de Pagamento do Mês de Maio", 1.5f);
-        v.adicionarFuncionario(p);
-        v.adicionarFuncionario(d);
-        Empresa e = new Empresa("Empresa 1", 456, c,  0);
-        Parceiro j = new Parceiro("Empresa 2",1,c,0);
-        vendaAtacado g = new vendaAtacado("Venda de Lote de PCS", 20, 2, 2000f, j, e);
-        h.addOcorrencia(v);
-        h.addOcorrencia(g);
-        Filial f = new Filial(h);
-        assertEquals(423f,f.obterLucroFinal());
-    }
 
     @Test
     void deveRetornarErroQtdDependentesCoordenadorNulo(){
@@ -149,5 +110,48 @@ class FilialTest {
         p.addDependentes(p3);
         f.setCoordenadorLocal(p);
         assertEquals(2,f.qtdDepsAbonoCoordenadorLocal());
+    }
+
+    //Consulta 19
+    @Test
+    void deveImprimirApenasDiscriminativo(){
+        Historico h = new Historico();
+        folhaPagamento v = new folhaPagamento("Folha de Pagamento do Mês de Maio", 1.5f);
+        Cidade c = new Cidade ("Juiz de Fora", "JF",new Estado("Minas Gerais", "MG", new Pais("Brasil","BR")));
+        Empresa e = new Empresa("Empresa 1", 456, c,  0);
+        Parceiro j = new Parceiro("Empresa 2",1,c,0);
+        vendaAtacado g = new vendaAtacado("Venda de Lote de PCS", 20, 2, 2000f, j, e);
+        h.addOcorrencia(v);
+        h.addOcorrencia(g);
+        Filial f = new Filial(h);
+        assertEquals("Folha de Pagamento do Mês de Maio\nVenda de Lote de PCS\n",f.imprimirApenasDiscriminativo());
+    }
+
+    //Consultas 20
+
+    @Test
+    void deveCalcularLucroFinal(){
+        Historico h = new Historico();
+        Cidade c = new Cidade ("Juiz de Fora", "JF",new Estado("Minas Gerais", "MG", new Pais("Brasil","BR")));
+        Funcionario p = new Funcionario("Fernando Silva",123,c,1412.0f,100.0f);
+        Dependente p1 = new Dependente("João Srbek",1,c,17);
+        Dependente p2 = new Dependente("Caua Moreno",2,c,18);
+        Dependente p3 = new Dependente("Pedro Moreno Srbek",3,c,19);
+        p.addDependentes(p1);
+        p.addDependentes(p2);
+        p.addDependentes(p3);
+        Funcionario d = new Funcionario("Carlos",456,c,1412.0f,0.0f);
+        Dependente d1 = new Dependente("Carlos Junior",4,c,17);
+        d.addDependentes(d1);
+        folhaPagamento v = new folhaPagamento("Folha de Pagamento do Mês de Maio", 1.5f);
+        v.adicionarFuncionario(p);
+        v.adicionarFuncionario(d);
+        Empresa e = new Empresa("Empresa 1", 456, c,  0);
+        Parceiro j = new Parceiro("Empresa 2",1,c,0);
+        vendaAtacado g = new vendaAtacado("Venda de Lote de PCS", 20, 2, 2000f, j, e);
+        h.addOcorrencia(v);
+        h.addOcorrencia(g);
+        Filial f = new Filial(h);
+        assertEquals(423f,f.obterLucroFinal());
     }
 }
